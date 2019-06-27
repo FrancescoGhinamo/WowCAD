624d86166edb57ef6c56e1b8d775025a6a8f8921
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-06-27 16:57:09 +0200

    Update Testing.java

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -1,12 +1,28 @@
 package testing;
 
+import java.io.File;
+import java.io.IOException;
+
 import wowcad.backend.beam.drawing.Drawing;
+import wowcad.backend.beam.drawing.JPEGExporter;
+import wowcad.backend.beam.shapes.Point;
+import wowcad.backend.beam.shapes.Segment;
 
 public class Testing {
 
 	public static void main(String[] args) {
 		Drawing d = new Drawing("pippo");
-		d.exportAsJPEG("C:\\Users\\franc\\OneDrive\\Desktop\\test.jpeg");
+		d.addPrimitive(new Point("", 10, 10));
+		d.addPrimitive(new Segment("test", new Point("", 5, 5), new Point("", 10, 5)));
+		JPEGExporter exp = new JPEGExporter(d, 5);
+		exp.drawCartesianAxis();
+		exp.drawShapes();
+		try {
+			exp.exportJpegImage(new File("C:\\Users\\franc\\OneDrive\\Desktop\\test.jpeg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
