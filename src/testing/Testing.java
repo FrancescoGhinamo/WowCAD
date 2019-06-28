9f2fc61cd42bb6a52fe05e7f8c5c488b593f946a
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-06-28 15:46:09 +0200

    Update Testing.java

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -5,17 +5,26 @@ import java.io.IOException;
 
 import wowcad.backend.beam.drawing.Drawing;
 import wowcad.backend.beam.drawing.JPEGExporter;
+import wowcad.backend.beam.shapes.Ellipse;
 import wowcad.backend.beam.shapes.Point;
+import wowcad.backend.beam.shapes.Polygon;
+import wowcad.backend.beam.shapes.Polyline;
 import wowcad.backend.beam.shapes.Segment;
 
 public class Testing {
 
 	public static void main(String[] args) {
 		Drawing d = new Drawing("pippo");
-		d.addPrimitive(new Point("", 10, 10));
+		d.addPrimitive(new Point("", 500, 500));
 		d.addPrimitive(new Segment("test", new Point("", 5, 5), new Point("", 10, 5)));
+		d.addPrimitive(new Ellipse("pippo", new Point("", 10, 10), 20, 10));
+		Polyline p = new Polygon("boh");
+		p.addPoint(new Point("", 30, 30));
+		p.addPoint(new Point("", -50, 0));
+		p.addPoint(new Point("", 0, 0));
+		d.addPrimitive(p);
 		JPEGExporter exp = new JPEGExporter(d, 5);
-		exp.drawCartesianAxis();
+//		exp.drawCartesianAxis();
 		exp.drawShapes();
 		try {
 			exp.exportJpegImage(new File("C:\\Users\\franc\\OneDrive\\Desktop\\test.jpeg"));
